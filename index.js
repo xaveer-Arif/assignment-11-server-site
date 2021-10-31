b5abfb84be1d1a115605a3117b876dd0fa9bc566
xaveerarif <xaveerarif@gmail.com>
2021-10-31 20:10:49 +0600

    heroku deploy  preparation

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId
 
 
-const port = 5000;
+const port = process.env.PORT ||  5000;
 // middleware
     app.use(cors());
     app.use(express.json())
 
 
-// console.log(uri)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vea3q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -29,15 +28,12 @@ async function run(){
         app.get('/services', async(req, res) => {
             const cursor = serviceCollection.find({});
             const services = await cursor.toArray();
-            // console.log('get')
             res.json(services)
         })
         // get api for order
         app.get('/orders/:id', async(req, res) => {
           const id = req.params.id
-            // console.log('get', id)
             const myOrder = await serviceCollection.find({email: id}).toArray()
-            console.log(myOrder)
             res.json(myOrder)
         })
         // get manage order
@@ -50,9 +46,7 @@ async function run(){
         // update
         app.put('/update/:id' , async(req, res) => {
             const id = req.params.id
-            // console.log(req.params.id)
             const updateInfo = req.body;
-            console.log(updateInfo)
             const filter = {_id: ObjectId(id)};
             const updateService = await serviceCollection.updateOne(filter, {
                 $set:{
@@ -60,7 +54,6 @@ async function run(){
                     status: "pending"
                 }
             });
-            // console.log(updateService)
             res.send(updateService)
         })
      
@@ -69,8 +62,7 @@ async function run(){
         app.post('/services',async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
-            // console.log('hit the post')
-            // res.json(result)
+           
             res.json(result)
         })
     }
@@ -87,9 +79,3 @@ app.get('/', async(req, res) => {
 app.listen(port, () => {
     console.log('listnitg port' ,port)
 })
-
-
-
-
-
-//...........................................
